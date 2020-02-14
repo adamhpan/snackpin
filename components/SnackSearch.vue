ca9a39c5f12c9b189cd515eb5fce87cdf622749c
Adam Pan <adam@freshprints.com>
2020-02-14 13:40:46 -0500

    Search feature soon

--- components/SnackSearch.vue
+++ components/SnackSearch.vue
@@ -4,7 +4,7 @@
       <label class="pr-2" for="inputSearch">
         <i class="fal fa-search"></i>
       </label>
-      <input @input="searchSnacks($event)" class="form-control" type="text">
+      <input @input="searchSnacks($event)" class="form-control" type="text" placeholder="Search feature soon!" disabled>
     </div>
   </div>
 </template>
